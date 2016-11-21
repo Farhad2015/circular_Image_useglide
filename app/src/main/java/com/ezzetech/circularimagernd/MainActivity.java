7970aac3ade12af3f0232913f51e31f46c7d1a42
Farhad2015 <mrforhad1@gmail.com>
2016-11-21 17:34:54 +0600

    Create Project and First commit

---
+++ app/src/main/java/com/ezzetech/circularimagernd/MainActivity.java
@@ -0,0 +1,33 @@
+package com.ezzetech.circularimagernd;
+
+import android.graphics.Bitmap;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.iv_showimage);
+        String url = "http://hdwallpapershdpics.com/wp-content/uploads/2016/10/nature-spring-forest-park-6452.jpg";
+        Glide.with(getApplicationContext()).load(url).asBitmap().centerCrop().into(new BitmapImageViewTarget(imageView) {
+            @Override
+            protected void setResource(Bitmap resource) {
+                RoundedBitmapDrawable circularBitmapDrawable =
+                        RoundedBitmapDrawableFactory.create(getApplicationContext().getResources(), resource);
+                circularBitmapDrawable.setCircular(true);
+                imageView.setImageDrawable(circularBitmapDrawable);
+            }
+        });
+    }
+}
